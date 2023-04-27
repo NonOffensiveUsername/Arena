00d9c072d07718ee0972bb7625a429620b1e6a17
NonOffensiveUsername <jasonddanner@protonmail.com>
2023-04-27 04:15:19 -0500

    Add Bresenham line implementation

--- util.py
+++ util.py
@@ -2,63 +2,32 @@ import math
 import random
 from copy import deepcopy
 
-# Given two points, return a list of points representing a line between them
-# TODO: Heavy cleanup, more thorough testing, simplification
-#def get_line(x1, y1, x2, y2):
-#
-#	# Handling the special case of vertical lines
-#	# TODO: fix this, can currently see through walls directly north
-#	if x1 == x2:
-#		result = []
-#		for i in range(y1, y2 + 1):
-#			result.append(Vec2(x1, i))
-#		return result
-#
-#	slope = (y2 - y1) / (x2 - x1)
-#	x_offset, y_offset = x1, y1
-#	x1, y1 = 0, 0
-#	x2 -= x_offset
-#	y2 -= y_offset
-#
-#	# Translate and reflect the coordinates so the line passes through 0,0 and has a slope between 0 and 1
-#
-#	vertical_reflect = 1
-#	if y2 < y1:
-#		vertical_reflect = -1
-#		y2 *= -1
-#		slope *= -1
-#
-#	horizontal_reflect = 1
-#	if x2 < x1:
-#		horizontal_reflect = -1
-#		x2 *= -1
-#		slope *= -1
-#
-#	swap_coords = False
-#
-#	if slope > 1: 
-#		swap_coords = True
-#		x1, y1 = y1, x1
-#		x2, y2 = y2, x2
-#		slope = (y2 - y1) / (x2 - x1)
-#
-#	result = []
-#
-#	for i in range(0, x2 + 1):
-#		x = i
-#		y = round_half_down(i * slope)
-#		if swap_coords: x, y = y, x
-#		y *= vertical_reflect
-#		x *= horizontal_reflect
-#		x += x_offset
-#		y += y_offset
-#		result.append(Vec2(x, y))
-#
-#	return result
+def bresenham_line(x0, y0, x1, y1):
+	dx = abs(x1 - x0)
+	dy = abs(y1 - y0)
 
-# Rounds to nearest integer, rounds ties down
-def round_half_down(num):
-	return math.ceil(num -.5)
+	if steep := dy > dx:
+		dx, dy = dy, dx
+		x0, y0 = y0, x0
+		x1, y1 = y1, x1
+
+	x, y = x0, y0
+	err = 2 * dy - dx
+	x_sign = 1 if x0 < x1 else -1
+	y_sign = 1 if y0 < y1 else -1
+
+	points = []
+	for i in range(dx + 1):
+		points.append((y, x) if steep else (x, y))
+		if err > 0:
+			y += y_sign
+			err += 2 * (dy - dx)
+		else:
+			err += 2 * dy
+
+		x += x_sign
+
+	return points
 
 def dist_between(a, b):
 	return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
@@ -95,21 +64,3 @@ def dict_overwrite(a, b):
 			else:
 				new_dict[k] = dict_overwrite(new_dict[k], b[k])
 	return new_dict
-
-#if __name__ == "__main__":
-#	a = {
-#		"test": 5,
-#		"test2": 7,
-#		"test3": {
-#			"woag": 99,
-#			"woazers": 3
-#		}
-#	}
-#	b = {
-#		"test2": 9,
-#		"test3": {
-#			"woazers": 2
-#		}
-#	}
-#	c = dict_overwrite(a, b)
-#	print(c)
